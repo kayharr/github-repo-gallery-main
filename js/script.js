8baede985bb14edddba9a11797836ed5af109b82
Kayla Baca <119377119+kayharr@users.noreply.github.com>
2024-09-15 11:54:04 -0500

    pulled user info and repos from api to populate page

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 const overview = document.querySelector(".overview");
 const username = "kayharr";
+const repoList = document.querySelector(".repo-list");
 
 const gitUserData = async function () {
     const res = await fetch(`https://api.github.com/users/${username}`);
     const data = await res.json();
-    // console.log(data);
     userInfo(data);
 };
 
@@ -25,5 +25,22 @@ const userInfo = function (data) {
     </div>
     `;
     overview.append(newDiv);
+   
 };
 
+const repoFetch = async function () {
+    const fetchRepo = await fetch(`https://api.github.com/users/${username}/repos?sort=updated&per_page=100`);
+    const repoData = await fetchRepo.json(); 
+    repoInfo(repoData);
+};
+
+repoFetch();
+
+const repoInfo = function (repos) {
+    for (const repo of repos) {
+        const repoItem = document.createElement("li");
+        repoItem.classList.add("repo");
+        repoItem.innerHTML = `<h3> ${repo.name}<h3>`;
+        repoList.append(repoItem);
+    }
+};
