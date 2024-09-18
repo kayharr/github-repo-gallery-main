353bd45269fadf9ce307d945cb9af96f971da6d9
Kayla Baca <119377119+kayharr@users.noreply.github.com>
2024-09-17 21:00:39 -0500

    pulled specific repo data from github, added event listener to open repo info boxes

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 const overview = document.querySelector(".overview");
 const username = "kayharr";
 const repoList = document.querySelector(".repo-list");
+const repoGroup = document.querySelector(".repos");
+const specificRepo = document.querySelector(".repo-data");
 
 const gitUserData = async function () {
     const res = await fetch(`https://api.github.com/users/${username}`);
@@ -24,19 +26,18 @@ const userInfo = function (data) {
       <p><strong>Number of public repos:</strong> ${data.public_repos}</p>
     </div>
     `;
-    overview.append(newDiv);
-   
+    overview.append(newDiv); 
 };
 
 const repoFetch = async function () {
     const fetchRepo = await fetch(`https://api.github.com/users/${username}/repos?sort=updated&per_page=100`);
     const repoData = await fetchRepo.json(); 
-    repoInfo(repoData);
+    repoDetails(repoData);
 };
 
 repoFetch();
 
-const repoInfo = function (repos) {
+const repoDetails = function (repos) {
     for (const repo of repos) {
         const repoItem = document.createElement("li");
         repoItem.classList.add("repo");
@@ -44,3 +45,42 @@ const repoInfo = function (repos) {
         repoList.append(repoItem);
     }
 };
+
+repoList.addEventListener("click", function (e) {
+    if (e.target.matches("h3")) {
+        const repoName = e.target.innerText;
+        repoPullInfo(repoName);
+    }
+});
+
+const repoPullInfo = async function (repoName) {
+    const grabRepos = await fetch(`https://api.github.com/repos/${username}/${repoName}`);
+    const repoInfo = await grabRepos.json();
+    console.log(repoInfo);
+
+    const fetchLanguages = await fetch(repoInfo.languages_url);
+    const languageData = await fetchLanguages.json();
+    console.log(languageData);
+
+    const languages = [];
+    for (const language in languageData) {
+        languages.push(language);
+    }
+    // console.log(languages);
+    displayRepo(repoInfo, languages);
+};
+
+const displayRepo = function (repoInfo, languages) {
+    specificRepo.innerHTML = "";
+    specificRepo.classList.remove("hide");
+    repoGroup.classList.add("hide");
+    const div = document.createElement("div");
+    div.innerHTML = `
+    <h3>Name: ${repoInfo.name}</h3>
+    <p>Description: ${repoInfo.description}</p>
+    <p>Default Branch: ${repoInfo.default_branch}</p>
+    <p>Languages: ${languages.join(", ")}</p>
+    <a class="visit" href="${repoInfo.html_url}" target="_blank" rel="noreferrer noopener">View Repo on GitHub!</a>`;
+    specificRepo.append(div);
+};
+
